0d63f9f072c752b7d034a52551ccf5c2c389c9da
hashcode <boniface@hashcode.zm>
2016-04-14 20:56:50 +0200

    Added Tests for Settings and an App Junit Test Suite

--- app/src/main/java/zm/hashcode/hashdroidpvt/ActivateActivity.java
+++ app/src/main/java/zm/hashcode/hashdroidpvt/ActivateActivity.java
@@ -2,26 +2,41 @@ package zm.hashcode.hashdroidpvt;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class ActivateActivity extends AppCompatActivity {
+    private  EditText emailAddress;
+    private  EditText password;
+    private  EditText orgcode;
+    private  Button activateButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activate);
-        TextView loginScreen = (TextView) findViewById(R.id.link_to_login);
+        emailAddress=(EditText)findViewById(R.id.activate_emailAddress);
+        password=(EditText) findViewById(R.id.activate_password);
+        orgcode=(EditText) findViewById(R.id.activate_organisationCode);
+        activateButton = (Button) findViewById(R.id.activate_activateButton);
 
-        // Listening to Login Screen link
-        assert loginScreen != null;
-        loginScreen.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View arg0) {
-                // Closing registration screen
-                // Switching to Login Screen/closing register screen
-                finish();
+        activateButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Do something in response to button click
+                Log.d("EMAIL",emailAddress.getText().toString());
+                Log.d("PASS",password.getText().toString());
+                Log.d("CODE",orgcode.getText().toString());
+
             }
         });
+
+
+
+
     }
 }
